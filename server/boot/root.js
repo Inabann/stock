f36ab2ebf74748bc1ff32ec8494b45e9dd486ece
inaban <25hbaam@gmail.com>
2017-05-30 23:40:36 -0500

    0.2

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,10 +1,10 @@
-'use strict';
+// 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', (req, res)=>{
-    res.send('hola mundo :v');
-  });
-  server.use(router);
-};
+// module.exports = function(server) {
+//   // Install a `/` route that returns server status
+//   var router = server.loopback.Router();
+//   router.get('/', (req, res)=>{
+//     res.send('hola mundo :v');
+//   });
+//   server.use(router);
+// };
